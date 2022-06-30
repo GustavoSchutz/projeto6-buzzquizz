189d63b54788e10c59edb72cb9b407e89a2f7452
GustavoSchutz <gutofelipe8@gmail.com>
2022-06-30 16:21:09 -0300

    Layout lista de quizzes pronto

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -1,3 +1,12 @@
 function criacaoDeQuizz (){
 
 }
+function getQuizzlist() {
+    const promessa = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+    promessa.then(writeQuizzlist);
+    promessa.catch(alerError);
+}
+function writeQuizzlist(quizzList) {
+    console.log(quizzList.data);
+}
+getQuizzlist()
