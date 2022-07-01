4293afc01f522a373a234ad25995fb0da112adfc
gisele <giselesilva2311@gmail.com>
2022-06-30 23:14:59 -0300

    inicio da tela 3

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -1,6 +1,15 @@
 function criacaoDeQuizz (){
-
+    document.querySelector(".tela1").classList.add(".apaga");
+    document.querySelector(".tela3").classList.remove(".apaga");
+    console.log("ola")
 }
+
+function prosseguirParaPergunta(){
+    document.querySelector(".tela1").classList.add(".apaga");
+    document.querySelector(".tela3").classList.add(".apaga");
+    document.querySelector(".tela3-1").classList.remove(".apaga");
+}
+
 function getQuizzlist() {
     const promessa = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
     promessa.then(writeQuizzlist);
