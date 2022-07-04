d40efd472c4c599c73c7f57d3368000bb5544d8e
GustavoSchutz <gutofelipe8@gmail.com>
2022-07-04 01:55:24 -0300

    adicionei as funções que geram as perguntas. Ajustes no css e html.

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -1,4 +1,6 @@
 let quiz;
+let qtdPerguntas;
+let qtdNiveis;
 
 function criacaoDeQuizz (){
     document.querySelector(".tela1").classList.add(".apaga");
@@ -51,15 +53,65 @@ function renderizarQuiz() {
 `;
 }
 }
+
+
 function tela3a() {
     let isValidTitle = tituloDoQuiz.checkValidity();
     let isValidImageURL = imagemDoQuiz.checkValidity();
     let isValidQuestionAmount = quantidadePerguntas.checkValidity();
     let isValidLevelAmount = quantidadeNiveis.checkValidity();
     if (isValidTitle && isValidImageURL && isValidQuestionAmount && isValidLevelAmount) {
-        console.log('deucerto')
+        gerarPerguntas();
     }else {
-        alert("Dados não estão certos")
-        document.getElementById("tituloDoQuiz").toggleClass(".errou")
+        alert("Dados não estão certos");
+        // document.getElementById("tituloDoQuiz").toggleClass(".errou") rodar audio do faustao
     }
+}
+function gerarPerguntas() {
+    qtdPerguntas = document.getElementById("quantidadePerguntas").value;
+
+    qtdNiveis = document.getElementById("quantidadeNiveis").value;
+
+    geradorPerguntas();
+    // geradorNiveis();
+}
+function geradorPerguntas() {
+    console.log("gerou?")
+    console.log(qtdPerguntas)
+    const perguntas = document.querySelector(".criandoPerguntas");
+    perguntas.innerHTML = '';
+  
+    for (let i = 1; i <= qtdPerguntas; i++) {
+  
+        perguntas.innerHTML += `
+        <div id="pergunta${i}" class="pergunta">
+                    <div class="botaoTartaruga">
+                        <h4>Pergunta ${i}</h4>
+                        <span>teste</span>
+                    </div>
+                    <div class="tartaruga">
+                        <input id="textoPergunta${i}" minlength="20" type="text" placeholder="Texto da pergunta" required>
+                        <input id="cor${i}" type="color" placeholder="Cor de fundo da pergunta" required>
+                        <h4>Resposta Correta</h4>
+                        <input id="respostaCorretaPergunta${i}" type="text" placeholder="Resposta correta" required>
+                        <input id="imagemCorreta${i}" type="url" placeholder="URL da imagem" required>
+                        <h4>Respostas incorretas</h4>
+                        <input id="respostaIncorreta${i}" type="text" placeholder="Resposta incorreta 1" required>
+                        <input type="url" placeholder="URL da imagem 1" required>
+                        <input id="respostaIncorreta${i}" type="text" placeholder="Resposta incorreta 2">
+                        <input type="url" placeholder="URL da imagem 2">
+                        <input id="respostaIncorreta${i}" type="text" placeholder="Resposta incorreta 3">
+                        <input type="url" placeholder="URL da imagem 3">    
+                    </div>
+                    
+                </div>
+  `;
+  }
+
+  perguntas.innerHTML += `<button class="prosseguir" onclick="tela3b()">Prosseguir para criar níveis</button>`;
+
+}
+
+function tela3b() {
+
 }
